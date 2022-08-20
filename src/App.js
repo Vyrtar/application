22b0040ec9800b48c9f9584fb15373dd9ebc8827
sam <sam@DESKTOP-7GFKDTB.localdomain>
2022-08-20 13:05:16 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
+
 import './App.css';
+import { Navigation } from './components/navigation';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="flex">
+    <BrowserRouter>
+      <Navigation />
+          <Routes>
+            <Route path="/thepage" element={<Home />} />
+            <Route path="/Home" element={<Home />} />
+            <Route path="/Fotos" element={<Fotos />} />
+            <Route path="/Hobbies" element={<Videos />} />
+            <Route path="/Blogs" element={<Blogs />} />
+        </Routes >
+  </BrowserRouter>
+  </div>
   );
 }
 
---
+++ src/components/navigation.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+import { string } from 'yargs';
+
+export const Navigation = () => {
+
+
+    return (
+        <>
+            
+           
+        </>
+    )
+} 
