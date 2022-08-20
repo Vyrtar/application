9e9ce6a9099feb4a3fa196209fdc70eaee7d1bb3
sam <sam@DESKTOP-7GFKDTB.localdomain>
2022-08-20 13:13:14 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -6,16 +6,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="flex">
-    <BrowserRouter>
-      <Navigation />
-          <Routes>
-            <Route path="/thepage" element={<Home />} />
-            <Route path="/Home" element={<Home />} />
-            <Route path="/Fotos" element={<Fotos />} />
-            <Route path="/Hobbies" element={<Videos />} />
-            <Route path="/Blogs" element={<Blogs />} />
-        </Routes >
-  </BrowserRouter>
+      <p>hello world</p>
   </div>
   );
 }
